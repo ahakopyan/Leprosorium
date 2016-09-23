f81c76eda867391f66aa827c2fd7c77d1c3a79bf
Араик Акопян <araikakopan@MacBook-Pro-Araik.local>
2016-09-24 01:09:44 +0300

    Add/details/...handler

--- app.rb
+++ app.rb
@@ -63,3 +63,12 @@ post '/new' do
 
 	redirect to '/'
 end
+
+# вывод информации о посте
+# обработчик для details
+
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	erb "Displaying information for post with id #{post_id}"
+end
