851d62abed56ff280495223b83ffc8c3d470a80c
Араик Акопян <araikakopan@MacBook-Pro-Araik.local>
2016-09-23 23:55:41 +0300

    Show latest posts

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ configure do
 get '/' do
 	# выбираем список постов из БД
 
-	results = @db.execute 'select * from Post order byid desc'
+	@results = @db.execute 'select * from Posts order by id desc'
 	erb :index
 end
 
